817eb5bf6aa4d7512add5046ee524228fd909ea5
Beck Boan <beckmboan@gmail.com>
2024-07-28 14:10:43 -0400

    Added logout, login, and sign up server actions. Began on sign up page.

---
+++ src/app/(auth)/signup/SignUpForm.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+interface SignUpFormProps {}
+
+const SignUpForm: React.FC<SignUpFormProps> = ({}) => {
+  const form = useForm<SignUpValues>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: SignUpValues) {
+    //
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Username" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          Create account
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default SignUpForm;
---
+++ src/app/(auth)/signup/actions.ts
@@ -0,0 +1,87 @@
+"use server";
+
+import { lucia } from "@/auth";
+import prisma from "@/lib/prisma";
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import { hash } from "@node-rs/argon2";
+import { generateIdFromEntropySize } from "lucia";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import { isRedirectError } from "next/dist/client/components/redirect";
+
+export async function signUp(
+  credentials: SignUpValues,
+): Promise<{ error: string }> {
+  try {
+    const { username, email, password } = signUpSchema.parse(credentials);
+
+    const passwordHash = await hash(password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
+
+    const userId = generateIdFromEntropySize(10);
+
+    //Check existing username in db
+    const existingUsername = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingUsername) {
+      return {
+        error: "Username already taken",
+      };
+    }
+
+    //Check existing email in db
+    const existingEmail = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingEmail) {
+      return {
+        error: "Email already taken",
+      };
+    }
+
+    await prisma.user.create({
+      data: {
+        id: userId,
+        username,
+        displayName: username,
+        email,
+        passwordHash,
+      },
+    });
+
+    // Create session and cookie
+    const session = await lucia.createSession(userId, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+
+    // Because we return Never, we don't need to return an error string Promise
+    return redirect("/");
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    console.error(error);
+    return {
+      error: "Something unexpected went wrong. Please try again later",
+    };
+  }
+}
