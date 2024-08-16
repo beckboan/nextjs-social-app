6e9e9d0a78ff372d6cb2070ddef7924ebbff1278
Beck Boan <beckmboan@gmail.com>
2024-08-16 17:15:33 -0400

    delete post cach modification

---
+++ src/components/posts/actions.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/types";
+import React from "react";
+
+const deletePost = async (id: string) => {
+  const { user } = await validateRequest();
+  if (!user) throw new Error("Unauthorized");
+
+  const post = await prisma.post.findUnique({
+    where: { id },
+  });
+
+  if (!post) throw new Error("Post not found");
+
+  //Need to make sure that the user patches the post userID
+
+  if (post.userId !== user.id) throw new Error("Unauthorized");
+
+  const deletedPost = await prisma.post.delete({
+    where: { id },
+    include: postDataInclude,
+  });
+
+  return deletedPost;
+};
+
+export default deletePost;
