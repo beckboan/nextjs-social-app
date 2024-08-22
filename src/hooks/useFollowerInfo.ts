d1d5ba9093b3012971bacbbe9c5a08febb8f1515
Beck Boan <beckmboan@gmail.com>
2024-08-21 20:52:48 -0400

    Hook for getting follower info

---
+++ src/hooks/useFollowerInfo.ts
@@ -0,0 +1,16 @@
+import kyInstance from "@/lib/ky";
+import { FollowerInfo } from "@/lib/types";
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+
+export const useFollowerInfo = (userId: string, initialState: FollowerInfo) => {
+  const query = useQuery({
+    queryKey: ["follower-info", userId],
+    queryFn: () =>
+      kyInstance.get(`/api/users/${userId}/followers`).json<FollowerInfo>(),
+    initialData: initialState,
+    staleTime: Infinity,
+  });
+
+  return query;
+};
