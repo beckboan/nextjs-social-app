a23906c22a7b6391e94830e9fc26a35e563466f9
Beck Boan <beckmboan@gmail.com>
2024-08-23 10:51:58 -0400

    Follower count client component

---
+++ src/components/FollowerCount.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useFollowerInfo } from "@/hooks/useFollowerInfo";
+import { FollowerInfo } from "@/lib/types";
+import { formatNumber } from "@/lib/utils";
+import React from "react";
+
+interface FollowerCountProps {
+  userId: string;
+  initialState: FollowerInfo;
+}
+
+const FollowerCount: React.FC<FollowerCountProps> = ({
+  userId,
+  initialState,
+}) => {
+  const { data } = useFollowerInfo(userId, initialState);
+
+  return (
+    <span>
+      Followers:{" "}
+      <span className="font-semibold">{formatNumber(data.followers)}</span>
+    </span>
+  );
+};
+
+export default FollowerCount;
